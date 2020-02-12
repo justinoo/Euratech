342df14852c524b4e826184627009bc55259f045
MattCornicDev <glgharib@gmail.com>
2020-02-12 14:32:38 +0100

    Formulaire contact

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class IndexController extends AbstractController
 {
@@ -22,10 +29,41 @@ class IndexController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(Request $request, ObjectManager $manager)
-       
-    {
+    public function create(Request $request, EntityManagerInterface $manager){
          $contact = new Contact();
-        return $this->render('index/contact.html.twig');
+
+         $form = $this->createFormBuilder($contact)
+                      ->add('prenom', TextType::class, [
+                          'attr' => [
+                              'placeholder' => "prenom",
+                              
+                          ]
+                      ])
+                      ->add('commune', TextType::class, [
+                          'attr' => [
+                              'placeholder' => "commune",
+                              
+                          ]
+                      ])
+                      ->add('email', EmailType::class, [
+                          'attr' => [
+                              'placeholder' => "Mail",
+                              
+                         ]
+                      ])
+                      ->add('message', TextareaType::class, [
+                          'attr' => [
+                              'placeholder' => "Message",
+                              
+                          ]
+                      ])
+                      ->add('save', SubmitType::class, [
+                          'label' => 'Enregistrer'
+                      ])
+                      ->getForm();
+
+        return $this->render('index/contact.html.twig', [
+            'formContact' => $form->createView()
+        ]);
     }
 }
