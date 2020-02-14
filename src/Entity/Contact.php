069a59bee421a3ebaf8279b71ed44be3ec3560db
MattCornicDev <glgharib@gmail.com>
2020-02-14 09:47:41 +0100

    formulaires

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraint as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
