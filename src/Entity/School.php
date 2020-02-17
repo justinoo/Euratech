81412f09d4f9da581c46c5b10911141b014a7f61
Nicolas Ravain <nicolasravain59@gmail.com>
2020-02-17 15:31:36 +0100

    formulaire contact ecoles

---
+++ src/Entity/School.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SchoolRepository")
+ */
+class School
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomref;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenomref;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $schoolname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nbrestudent;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomref(): ?string
+    {
+        return $this->nomref;
+    }
+
+    public function setNomref(string $nomref): self
+    {
+        $this->nomref = $nomref;
+
+        return $this;
+    }
+
+    public function getPrenomref(): ?string
+    {
+        return $this->prenomref;
+    }
+
+    public function setPrenomref(string $prenomref): self
+    {
+        $this->prenomref = $prenomref;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSchoolname(): ?string
+    {
+        return $this->schoolname;
+    }
+
+    public function setSchoolname(string $schoolname): self
+    {
+        $this->schoolname = $schoolname;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getNbrestudent(): ?string
+    {
+        return $this->nbrestudent;
+    }
+
+    public function setNbrestudent(string $nbrestudent): self
+    {
+        $this->nbrestudent = $nbrestudent;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
---
+++ src/Form/SchoolType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\School;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SchoolType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nomref')
+            ->add('prenomref')
+            ->add('email')
+            ->add('schoolname')
+            ->add('ville')
+            ->add('nbrestudent')
+            ->add('message')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => School::class,
+        ]);
+    }
+}
